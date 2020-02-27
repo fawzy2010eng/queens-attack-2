c38a0eb915490e40b7fbb85130860db931a89791
ahmed fawzy <42780497+fawzy2010eng@users.noreply.github.com>
2020-02-27 09:02:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ function queensAttack(n, k, y_q, x_q, obstacles) {
         [],//upleft
         [],//dowleft
         [],//upright
+        [],//downright
     ];
+    
+    here we add the possible points in each direction for 
+    the main array of possibilities
     for(var i = x_q-1 ;i >= 1 ;i--){
         allpossiblesquare[0].push(new Point(y_q,i))  
     }    
+    
     for(var i = x_q+1 ;i <= n ;i++){
         allpossiblesquare[1].push(new Point(y_q,i))  
     }
+    
     for(var i = y_q+1 ; i <= n ; i++){
         allpossiblesquare[2].push(new Point(i,x_q))  
     }
@@ -39,13 +45,28 @@ function queensAttack(n, k, y_q, x_q, obstacles) {
                 i--
             }                                      
         }
-    }
-    for(var i = y_q+1;i<=n;i++){
-        for(var j = x_q+1;j<=n;j++){
-            
+    }  
+    
+    for(var i = 1;i< n-x_q+1;i++){
+        for(var j = 1;j<n-y_q+1;j++){
+            if(i == j){
+                allpossiblesquare[6].push(new Point(i+y_q,j+x_q))  
+            }  
         }
     }
+    
+    for(var i = x_q+1; i < n;i++){
+        for(var j = y_q-1; j >= 1; j--){
+            if((i + j) == (y_q+x_q)){
+                allpossiblesquare[7].push(new Point(j,i))  
+            }
+        }  
+    }
+    
+    
+    
+    
     console.log((allpossiblesquare))
 }
 
-queensAttack(5,0,2,4,[]);
+queensAttack(5,3,2,5,[]);
